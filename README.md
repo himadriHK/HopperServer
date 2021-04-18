f696f3d59956c643dbb6a31178381f1b88247f53
HimadriHK <himadri.hk@gmail.com>
2021-04-18 18:36:51 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# HopperServer
+# Server
+
+
---
+++ app.js
@@ -0,0 +1,9 @@
+'use strict';
+var net = require('net');
+
+var server = net.createServer(function (socket) {
+	socket.write('Echo server\r\n');
+	socket.on("data", function (data) { console.log(data) });
+});
+
+server.listen(1337);
