2e68126fe228365791e59d4812cfb3115e467325
HimadriHK <himadri.hk@gmail.com>
2021-04-18 19:00:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var net = require('net');
 
 var server = net.createServer(function (socket) {
 	socket.write('Echo server\r\n');
+	socket.pipe(socket);
 	socket.on("data", function (data) { console.log(data) });
 });
 
